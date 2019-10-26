5bff1104d4d87609da6ab2ed8d07e835e44b94c1
Nikola Ciganović <nciganovic99@gmail.com>
2019-10-26 15:07:44 +0200

    heroku ready

--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django-heroku.settings(locals())
